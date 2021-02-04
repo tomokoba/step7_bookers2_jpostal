682b9a26ad6fa5231d3a6c72050f21f3a5fbb3c2
tomokoba <tomokoba64@gmail.com>
2021-02-04 08:28:52 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy] 
+    member do
+     get :following, :followers
+    end
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
