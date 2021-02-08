a0a09143f2c4abb25d273695ff2df517e7223baa
tomokoba <tomokoba64@gmail.com>
2021-02-08 12:05:45 +0000

    Initial Commit

--- Gemfile
+++ Gemfile
@@ -66,8 +66,8 @@ gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 
 gem 'jquery-rails'
+gem 'jp_prefecture'
 gem 'font-awesome-sass', '~> 5.13'
 gem 'bootstrap', '~> 4.5'
-gem 'jquery-rails'
 gem 'pry-byebug'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,15 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :email,
+      :name,
+      :postcode,
+      :prefecture_name,
+      :address_city,
+      :address_street,
+    ])
+  end
 end
