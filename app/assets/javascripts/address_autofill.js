f083524aaad3453e6e44b65f3fc9942111c355e9
tomokoba <tomokoba64@gmail.com>
2021-02-08 12:43:26 +0000

    Initial Commit

--- app/assets/javascripts/address_autofill.js
+++ app/assets/javascripts/address_autofill.js
@@ -3,8 +3,8 @@ $(function() {
     postcode: ['#user_postcode'],
     address: {
       '#user_prefecture_code': '%3',
-      '#user_address_city': '%4',
-      '#user_address_street': '%5%6%7',
+      '#user_address_city': '%4%5',
+      '#user_address_street': '%6%7',
     },
   });
 });
